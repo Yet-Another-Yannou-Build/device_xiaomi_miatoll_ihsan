f0e25bca42481e2c0ac91a87bcd4b2d9fa25abb3
Yann Renard <yannrenard1025@gmail.com>
2024-08-18 21:59:36 +0200

    miatoll: EvolutionX bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/voltage_miatoll.mk
+    $(LOCAL_DIR)/evolution_miatoll.mk
 
 COMMON_LUNCH_CHOICES := \
-    voltage_miatoll-ap1a-user \
-    voltage_miatoll-ap1a-userdebug \
-    voltage_miatoll-ap1a-eng
+    evolution_miatoll-ap1a-user \
+    evolution_miatoll-ap1a-userdebug \
+    evolution_miatoll-ap1a-eng
